bcd8ac516199434018b39fc6d23eed6f409d2c9d
mariofrong <sirisak.w@likemeasia.com>
2024-10-24 22:32:49 +0700

    feat: 🎸 changes font styles & changes homepage ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "swiper/css";
+import "swiper/css/pagination";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
