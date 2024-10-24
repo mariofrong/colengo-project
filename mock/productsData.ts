4986e3333123ed6c941dbd9cd9b4ec37d45b667e
mariofrong <sirisak.w@likemeasia.com>
2024-10-24 22:30:15 +0700

    feat: 🎸 create mock & types & utils folders

---
+++ mock/productsData.ts
@@ -0,0 +1,43 @@
+import { Product } from "@/types/product";
+
+export const products: Product[] = [
+  {
+    id: "1",
+    name: "Low dining tuinset 6 personen ALFA",
+    price: 8545,
+    image: "/images/products/furniture2.png",
+  },
+  {
+    id: "2",
+    name: "Luxe loungeset tuin KOEN",
+    price: 8545,
+    image: "/images/products/furniture1.png",
+    tag: "Bestseller",
+  },
+  {
+    id: "3",
+    name: "Luxe loungestoel teak BENTE",
+    price: 8545,
+    image: "/images/products/furniture3.png",
+    tag: "Musthave",
+  },
+  {
+    id: "4",
+    name: "Rond daybed buiten FLORENCE",
+    price: 8545,
+    image: "/images/products/furniture4.png",
+  },
+  {
+    id: "5",
+    name: "Rond loungestoel buiten FLORENCE",
+    price: 8545,
+    image: "/images/products/furniture3.png",
+    tag: "Musthave",
+  },
+  {
+    id: "6",
+    name: "Luxe dining buiten FLORENCE",
+    price: 8545,
+    image: "/images/products/furniture4.png",
+  },
+];
---
+++ utils/commons.ts
@@ -0,0 +1,6 @@
+export const BREAKPOINTS = {
+  smallMobile: 340,
+  mobile: 375,
+  tablet: 744,
+  desktop: 1680,
+};
